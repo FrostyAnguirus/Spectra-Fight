bb139924bd4ff4ac8d1dcc531a5b1d96ffe59389
FrostyAnguirus <julian.king28@student.tamdistrict.org>
2025-03-18 20:53:06 +0000

    invincibility frames

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Running the code
 
 Type 'npm run dev' in the Terminal (Press ctrl + shift + C to open a terminal)
+open up code with ctrl+shift+i
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,7 +6,9 @@ export class Game extends Scene {
     player2: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     cursors: Phaser.Types.Input.Keyboard.CursorKeys;
     player1Health = 100;
+    player1ImmunityFrames = 0;
     player2Health = 100;
+    player2ImmunityFrames = 0;
     healthText: any;
     keys: any;
     stars: any;
@@ -38,7 +40,7 @@ export class Game extends Scene {
 
         this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-        this.healthText = this.add.text(16, 16, 'P1: ' + this.player1Health + " | P2: " + this.player2Health, { fontSize: '32px', fill: '#000' });
+        this.healthText = this.add.text(16, 16, 'P1: ' + this.player1Health + " | P2: " + this.player2Health, { fontSize: '32px', color: '#000' });
 
 
         this.player1 = this.physics.add.sprite(100, 511, 'dude');
@@ -53,12 +55,14 @@ export class Game extends Scene {
         this.physics.add.collider(this.player2, this.platforms);
 
         let onPlayersCollide = (_player1: any, _player2: any) => {
-            if (this.keys.N.isDown/* player 2 attacking */) {
-                this.player1Health -= 1;
+            if (this.player1ImmunityFrames <= 0 /* out of immunity */ && this.keys.N.isDown/* player 2 attacking */) {
+                this.player1Health -= 10;
+                this.player1ImmunityFrames = 35; // make them immune for a bit
             }
 
-            if (this.keys.Q.isDown/* player 1 attacking */) {
-                this.player2Health -= 1;
+            if (this.player2ImmunityFrames <= 0 /* out of immunity */ && this.keys.Q.isDown/* player 1 attacking */) {
+                this.player2Health -= 10;
+                this.player2ImmunityFrames = 35; // make them immune for a bit
             }
             this.healthText.setText('P1: ' + this.player1Health + " | P2: " + this.player2Health);
         }
@@ -88,6 +92,23 @@ export class Game extends Scene {
     }
 
     update() {
+        if (this.player1ImmunityFrames > 0){
+            this.player1ImmunityFrames = this.player1ImmunityFrames - 1;
+        }
+        if (this.player1ImmunityFrames % 4 >= 2){ // every other immunity frame ( 0 1 2 3)
+            this.player1.setTint(0x888888);
+        } else {
+            this.player1.setTint(0xffffff)
+        }
+        
+        if (this.player2ImmunityFrames > 0){
+            this.player2ImmunityFrames = this.player2ImmunityFrames - 1;
+        }
+        if (this.player2ImmunityFrames % 4 >= 2){ // every other immunity frame ( 0 1 2 3)
+            this.player2.setTint(0x888888);
+        } else {
+            this.player2.setTint(0xf00000)
+        }
 
         // Player 1 (WASD)
         if (this.keys.A.isDown) {
